a5b3011fa9e575f68a2360095f686466329d9e9b
MrKazio <115210775+Dzban0@users.noreply.github.com>
2025-03-28 12:24:12 +0100

    Update AddProject.tsx

--- src/components/AddProject.tsx
+++ src/components/AddProject.tsx
@@ -49,7 +49,7 @@ const AddProject = (props: AddProjectProps) => {
         onClick={handleAdd}
         className="w-full px-4 py-2 bg-green-600 hover:bg-green-500 rounded text-white font-semibold transition duration-300"
       >
-        ➕ Dodaj projekt
+        Dodaj projekt
       </button>
     </div>
   );
